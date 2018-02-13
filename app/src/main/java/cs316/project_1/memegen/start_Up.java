872130fcdb9c977313fd819628913b6bfa421743
Dcb426 <Dcb13a@acu.edu>
2018-02-13 17:10:30 -0600

    Initial commit

---
+++ app/src/main/java/cs316/project_1/memegen/start_Up.java
@@ -0,0 +1,13 @@
+package cs316.project_1.memegen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class start_Up extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start__up);
+    }
+}
