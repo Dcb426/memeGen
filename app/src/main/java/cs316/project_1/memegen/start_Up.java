23adc20332567d56eb71a73038d93cd5e61f4a0f
Dcb426 <Dcb13a@acu.edu>
2018-02-13 18:57:48 -0600

    yeeeeeaaaaaaa

--- app/src/main/java/cs316/project_1/memegen/start_Up.java
+++ app/src/main/java/cs316/project_1/memegen/start_Up.java
@@ -1,13 +1,28 @@
 package cs316.project_1.memegen;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class start_Up extends AppCompatActivity {
 
+    // Creating button.
+    ImageButton Uploadpage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start__up);
+
+        //Move to UploadPage
+        final Intent moveToUploadPage = new Intent(this, upload_photos.class);
+        Uploadpage = (ImageButton) findViewById(R.id.UploadPicture);
+        Uploadpage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            startActivity(moveToUploadPage);
+            }
+        });
     }
 }
